dcab4de7f2e6ff8b2a292e86cc6c90d554c4f384
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2025-01-31 08:49:52 -0500

    defined a state struct, which it's fields store pointers to queries and a config of the current user and connection string to db.

--- state.go
+++ state.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	cfg "github.com/carlogy/rssfeedaggregator/internal/config"
+	"github.com/carlogy/rssfeedaggregator/internal/database"
 )
 
 type state struct {
+	db     *database.Queries
 	config *cfg.Config
 }
 
-func NewState(cfg *cfg.Config) state {
-	return state{config: cfg}
+func NewState(cfg *cfg.Config, db *database.Queries) state {
+	return state{config: cfg, db: db}
 }
 
 type command struct {
