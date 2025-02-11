59a0d389bdaa652fd7f9fdbfe37bb59145014c94
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2025-02-11 08:00:27 -0500

    setup schemas to use goose for table migrations for the feed_follows table. This table has an id as the Primary Key, and two foreign keys. One, user_id is the id column from users table. Two, feed_id is the id from feeds table. They are both set to Delete on cascade, so if a user or feed is deleted the feed_follow record will also be deleted.

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -3,8 +3,10 @@ CREATE TABLE feed_follows (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE feed_id INTEGER NOT NULL REFERENCES feeds (id) ON DELETE CASCADE ADD CONSTRAINT user_feed_pair UNIQUE (userid, feed_id)
+    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    feed_id INTEGER NOT NULL REFERENCES feeds (id) ON DELETE CASCADE,
+    CONSTRAINT User_Feed_Pair UNIQUE (user_id, feed_id)
 );
 
 -- +goose Down
-Drop Table feed_follows
+Drop Table feed_follows;
