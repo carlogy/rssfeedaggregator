2d442dd02fab87fd56f7b81beee70be87051c0ac
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2025-02-14 08:12:32 -0500

    Setup the feed follows queries to create a feed_follows record, get all the feed follow records, and delete a feed_follow based on  a provided url, and feed id.

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -1,4 +1,53 @@
--- Add a CreateFeedFollow query. It will be a deceptively complex SQL query. It should insert a feed follow record, but then return all the fields from the field follow as well as the names of the linked user and feed. I'll add a tip at the bottom of this lesson if you need it.
--- name CreateFeedFollow :one
+-- name: CreateFeedFollow :one
+WITH inserted_feed_follows AS (
 INSERT INTO
-    feed_follows ()
+    feed_follows(created_at, updated_at, user_id, feed_id)
+    VALUES (
+    $1,
+    $2,
+    $3,
+    $4
+    )
+    RETURNING *
+    )
+
+    SELECT
+        inserted_feed_follows.*,
+        feeds.name AS feed_name,
+        users.name AS user_name
+    FROM
+        inserted_feed_follows
+    INNER JOIN
+        feeds ON inserted_feed_follows.feed_id = feeds.id
+    INNER JOIN
+        users ON inserted_feed_follows.user_id = users.id;
+
+
+-- name: GetFeedFollowsForUser :many
+WITH get_feed_follows AS (
+SELECT
+    *
+FROM
+    feed_follows
+WHERE
+    feed_follows.user_id = $1
+)
+
+SELECT
+    get_feed_follows.*,
+    feeds.name AS feed_name,
+    users.name AS user_name
+FROM
+    get_feed_follows
+INNER JOIN feeds on get_feed_follows.feed_id = feeds.id
+INNER JOIN users on get_feed_follows.user_id = users.id;
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows
+USING feeds
+WHERE
+    feeds.id = feed_follows.feed_id
+AND
+    feed_follows.user_id = $1
+AND
+    feeds.url = $2;
