6fa871500573d8df06d178e20702134fa600b42e
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2025-02-19 07:53:18 -0500

    All of the SQL queries to insert or get feeds from the feeds table.

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -14,3 +14,48 @@ SELECT
     *
 FROM
     feeds;
+
+
+-- name: GetFeedByURL :one
+
+SELECT
+    *
+FROM
+    feeds
+WHERE
+    feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET
+    updated_at = $1,
+    last_fetched_at = $2
+WHERE
+    feeds.id = $3;
+
+
+-- name: GetNextFeedToFetch :one
+-- WITH oldest_fetched AS (
+-- SELECT
+--     f.last_fetched_at
+-- FROM
+--     feeds f
+-- ORDER BY f.last_fetched_at ASC NULLS LAST
+-- Limit 1
+-- )
+-- SELECT
+--     *
+-- From
+--     feeds f
+-- WHERE
+--     f.last_fetched_at IS NULL OR
+--     f.last_fetched_at <= (SELECT last_fetched_at FROM oldest_fetched)
+-- ORDER BY f.last_fetched_at ASC NULLS FIRST
+-- Limit 1;
+
+SELECT
+    *
+FROM
+    feeds f
+ORDER BY f.last_fetched_at ASC NULLS FIRST
+LIMIT 1;
