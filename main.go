f7323dea9b95eaa736c9de7a3329e30665b2669c
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2025-03-05 07:46:59 -0500

    updated main go file with all arg handlers

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	c "github.com/carlogy/rssfeedaggregator/internal/config"
+	database "github.com/carlogy/rssfeedaggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -14,15 +17,32 @@ func main() {
 		fmt.Println(err)
 	}
 
-	s := NewState(&cfg)
+	db, err := sql.Open("postgres", cfg.DbURL)
+
+	if err != nil {
+		fmt.Println("Experienced error connecting to db:\t", err)
+		os.Exit(1)
+	}
+
+	dbQueries := database.New(db)
+
+	s := NewState(&cfg, dbQueries)
 
 	commands := commands{
 		cmdMap: make(map[string]func(*state, command) error),
 	}
 
 	commands.register("login", handlerLogins)
-
-	fmt.Println(s, commands)
+	commands.register("register", handlerRegister)
+	commands.register("reset", handlerReset)
+	commands.register("users", handlerUsers)
+	commands.register("agg", handlerAgg)
+	commands.register("addfeed", middlewareLoggedIn(handlerAddFeed))
+	commands.register("feeds", handlerFeeds)
+	commands.register("follow", middlewareLoggedIn(handlerFollow))
+	commands.register("following", middlewareLoggedIn(handlerFollowing))
+	commands.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	commands.register("browse", middlewareLoggedIn(handlerBrowse))
 
 	Args := os.Args
 
@@ -40,13 +60,5 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	// newConfigRead, error := c.Read()
-
-	// if error != nil {
-	// 	fmt.Println(error)
-	// }
-
-	// // fmt.Println(newConfigRead.CurrentUserName, newConfigRead.DbURL)
 
 }
